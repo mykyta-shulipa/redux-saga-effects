8863206719d2cd913f924745016653b6adc0552a
Nikita Shulipa <nshulipa@gmail.com>
2017-02-19 10:47:45 +0200

    Implement first version

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import { effects } from 'redux-saga';
+
+export const actionChannel = effects.actionChannel;
+export const apply = effects.apply;
+export const call = effects.call;
+export const cancel = effects.cancel;
+export const cancelled = effects.cancelled;
+export const cps = effects.cps;
+export const flush = effects.flush;
+export const fork = effects.fork;
+export const join = effects.join;
+export const put = effects.put;
+export const race = effects.race;
+export const select = effects.select;
+export const spawn = effects.spawn;
+export const take = effects.take;
+export const takeEvery = effects.takeEvery;
+export const takeLatest = effects.takeLatest;
+export const takem = effects.takem;
+export const throttle = effects.throttle;
